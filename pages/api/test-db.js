4a213d6dcc876739f1e131e479299be7d765d0b4
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-11-04 04:47:08 -0400

    changes the db query for testing the connection to db

--- pages/api/test-db.js
+++ pages/api/test-db.js
@@ -2,7 +2,9 @@ import db from "./../../adapters/db"
 
 export default async function handler(req, res) {
   try {
-    const result = await db.query("select * from information_schema.tables;")
+    const result = await db.query(
+      "select COUNT(*) from information_schema.tables;"
+    )
     return res.json({ result })
   } catch (err) {
     console.error(err)
