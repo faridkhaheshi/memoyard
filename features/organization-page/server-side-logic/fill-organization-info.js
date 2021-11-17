3710ac57641c090ad3a2332e20b815f9c57bd50d
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-11-17 03:38:31 -0500

    removes console.logs

--- features/organization-page/server-side-logic/fill-organization-info.js
+++ features/organization-page/server-side-logic/fill-organization-info.js
@@ -4,21 +4,13 @@ const { baseUrl } = config
 const revalidationMins = 5
 
 const fillOrganizationInfo = async ({ params }) => {
-  try {
-    const { slug } = params
-    const orgResponse = await fetch(`${baseUrl}/api/organizations/${slug}`)
-    if (orgResponse.status !== 200) return { notFound: true }
-    const { organization } = await orgResponse.json()
-    return {
-      props: { organization },
-      revalidate: revalidationMins * 60,
-    }
-  } catch (err) {
-    console.error("error")
-    console.error(err)
-    return {
-      props: {},
-    }
+  const { slug } = params
+  const orgResponse = await fetch(`${baseUrl}/api/organizations/${slug}`)
+  if (orgResponse.status !== 200) return { notFound: true }
+  const { organization } = await orgResponse.json()
+  return {
+    props: { organization },
+    revalidate: revalidationMins * 60,
   }
 }
 
