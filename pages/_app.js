5c612ce4732f8b3df6dfef7da17c50b8af720f50
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-11-29 04:54:55 -0500

    adds the page load indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
+import Router from "next/router"
 import { DefaultSeo } from "next-seo"
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import { CacheProvider } from "@emotion/react"
+import NProgress from "nprogress"
+
 import { AuthContextProvider } from "../contexts/auth"
+
 import seoConfig from "../next-seo.config"
 
 import createEmotionCache from "../styles/create-emotion-cache"
 import theme from "../styles/theme"
 
 import "../styles/globals.scss"
+import "nprogress/nprogress.css"
 
 const clientSideEmotionCache = createEmotionCache()
+
+Router.events.on("routeChangeStart", () => NProgress.start())
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
 
 function MyApp({
   Component,
