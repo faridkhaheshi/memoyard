774b3b38f85a45ec357c4ecee9b5180fcaeb79d6
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-12-03 04:42:28 -0500

    adds the group field to the add subject form in panel

---
+++ features/panel/components/OrgSubjects/GroupSelector/GroupSelector.js
@@ -0,0 +1,31 @@
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import FormControl from "@mui/material/FormControl"
+import Select from "@mui/material/Select"
+import useOrgGroups from "../../../hooks/use-org-groups"
+
+const GroupSelector = ({ sx, id, value, onChange, label = "Class" }) => {
+  const { groups } = useOrgGroups()
+
+  return (
+    <FormControl sx={sx}>
+      <InputLabel id={`${id}-label`}>{label}</InputLabel>
+      <Select
+        labelId={`${id}-label`}
+        id={id}
+        label={label}
+        value={value || ""}
+        onChange={onChange}
+      >
+        {!!groups &&
+          groups.map(({ ex_id, name }) => (
+            <MenuItem key={ex_id} value={ex_id}>
+              {name}
+            </MenuItem>
+          ))}
+      </Select>
+    </FormControl>
+  )
+}
+
+export default GroupSelector
--- services/subjects/processors/index.js
+++ services/subjects/processors/index.js
@@ -1,3 +1,4 @@
 export { default as findOrgSubjects } from "./find-org-subjects"
 export { default as updateOrgSubject } from "./update-org-subject"
 export { default as addSubjectToOrg } from "./add-subject-to-org"
+export { default as addSubjectToGroup } from "./add-subject-to-group"
