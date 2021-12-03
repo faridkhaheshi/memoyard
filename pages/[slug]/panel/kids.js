6064d6a85238ad4d9ddb2b307c1efcb5c93d5785
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-12-03 06:40:05 -0500

    finishes the kids page in the panel

--- pages/[slug]/panel/kids.js
+++ pages/[slug]/panel/kids.js
@@ -4,8 +4,16 @@ import OrgSubjects from "../../../features/panel/components/OrgSubjects"
 
 const PanelKidsPage = () => (
   <PanelLayout>
-    <Typography variant="h4" component="h1" sx={{ marginBottom: "40px" }}>
+    <Typography variant="h4" component="h1" sx={{ marginBottom: "10px" }}>
       Kids
+    </Typography>
+    <Typography component="p">
+      Here you can add new kids to your school. Each kids belongs to a class.
+    </Typography>
+    <Typography component="p" sx={{ marginBottom: "10px" }}>
+      When you send a photo/video, you can specify the name of the kid/kids to
+      whom the media is related to. The memo will be available only to parents
+      of the kids you or your teachers specify while uploading the photo/video.
     </Typography>
     <OrgSubjects />
   </PanelLayout>
