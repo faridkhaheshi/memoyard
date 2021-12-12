0cb5b19d389aaf339449e2a0696153be95b34366
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-12-12 18:29:09 -0500

    completes the home page

--- features/home-page/components/MemoyardInfoCard/MemoyardInfoCard.js
+++ features/home-page/components/MemoyardInfoCard/MemoyardInfoCard.js
@@ -1,6 +1,4 @@
 import Typography from "@mui/material/Typography"
-import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
 import MemoPaper from "../../../../components/MemoPaper"
 
 const MemoyardInfoCard = () => (
@@ -10,8 +8,27 @@ const MemoyardInfoCard = () => (
     title="Memoyard"
     actionText="Let's Talk"
     actionProps={{ href: "mailto:hi@memoyard.com" }}
+    sx={{
+      color: "rgba(255,255,255,0.6)",
+      "& strong": { color: "white" },
+      "& h6": { color: "white" },
+    }}
   >
-    Hi
+    <Typography gutterBottom variant="h6">
+      What it does?
+    </Typography>
+    <Typography gutterBottom align="justify" mb={3}>
+      We create a <strong>private online album for daycares</strong> so that
+      they can <strong>share photos/videos</strong> of children with their
+      parents easily.
+    </Typography>
+    <Typography gutterBottom variant="h6">
+      How can I use it for my daycare?
+    </Typography>
+    <Typography gutterBottom align="justify">
+      Currenly, the system is being used by a limited number of daycares. If you
+      are interested, please send an email to us.
+    </Typography>
   </MemoPaper>
 )
 
