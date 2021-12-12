8a1d046d1892872aee8a3b12deb038cb1edf394a
Farid Khaheshi <farid.khaheshi@gmail.com>
2021-12-12 05:10:16 -0500

    separated the Fixed Aspect Ratio Paper into a separate component

---
+++ components/FixedAspectRatioPaper/FixedAspectRatioPaper.js
@@ -0,0 +1,35 @@
+import Box from "@mui/material/Box"
+import Paper from "@mui/material/Paper"
+
+const FixedAspectRatioPaper = ({
+  children,
+  width = "100%",
+  maxWidth = "100vh",
+  heightToWidthRatio = "100%",
+  ...otherProps
+}) => (
+  <Box sx={{ width, maxWidth }}>
+    <Box
+      sx={{
+        width: "100%",
+        paddingTop: heightToWidthRatio,
+        height: 0,
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Paper {...otherProps}>{children}</Paper>
+      </Box>
+    </Box>
+  </Box>
+)
+
+export default FixedAspectRatioPaper
--- features/organization-page/components/OrganizationContactCard/OrganizationContactCard.js
+++ features/organization-page/components/OrganizationContactCard/OrganizationContactCard.js
@@ -3,96 +3,74 @@ import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import CardActions from "@mui/material/CardActions"
 import CardContent from "@mui/material/CardContent"
-import Paper from "@mui/material/Paper"
+import FixedAspectRatioPaper from "../../../../components/FixedAspectRatioPaper"
 
 const OrganizationContactCard = ({ organization }) => (
-  <Box sx={{ width: "40vh", maxWidth: "100vw" }}>
-    <Box
-      elevation={4}
-      sx={{
-        width: "100%",
-        paddingTop: "150%",
-        height: 0,
-        position: "relative",
-      }}
-    >
+  <FixedAspectRatioPaper
+    heightToWidthRatio="150%"
+    width="40vh"
+    elevation={12}
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      height: "100%",
+      overflow: "scroll",
+      backgroundColor: "#9C528B",
+      borderRadius: "12px",
+      color: "white",
+    }}
+  >
+    <CardContent sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+      <Typography
+        gutterBottom
+        variant="h4"
+        component="h1"
+        align="center"
+        sx={{ margin: "64px 0 64px 0" }}
+      >
+        {organization.name}
+      </Typography>
+      <hr style={{ width: "100%" }} />
       <Box
         sx={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          padding: "32px 0 0 0",
         }}
       >
-        <Paper
-          elevation={12}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            height: "100%",
-            overflow: "scroll",
-            backgroundColor: "#9C528B",
-            borderRadius: "12px",
-            color: "white",
-          }}
-        >
-          <CardContent
-            sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}
-          >
-            <Typography
-              gutterBottom
-              variant="h4"
-              component="h1"
-              align="center"
-              sx={{ margin: "64px 0 64px 0" }}
-            >
-              {organization.name}
-            </Typography>
-            <hr style={{ width: "100%" }} />
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: "flex",
-                flexDirection: "column",
-                padding: "32px 0 0 0",
-              }}
-            >
-              <Typography gutterBottom align="justify">
-                Here you can view photos and videos of your child during their
-                time in school.
-              </Typography>
-            </Box>
-            <Typography
-              variant="caption"
-              component="small"
-              sx={{ color: "rgba(255,255,255, 0.6)" }}
-            >
-              * All photos and videos are shared privately. Only parents of the
-              children can access them.
-            </Typography>
-          </CardContent>
-          <CardActions
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              margin: "0 0 16px 0",
-            }}
-          >
-            <Button
-              size="large"
-              variant="outlined"
-              href="/"
-              sx={{ color: "white", borderColor: "white" }}
-            >
-              Visit Album
-            </Button>
-          </CardActions>
-        </Paper>
+        <Typography gutterBottom align="justify">
+          Here you can view photos and videos of your child during their time in
+          school.
+        </Typography>
       </Box>
-    </Box>
-  </Box>
+      <Typography
+        variant="caption"
+        component="small"
+        sx={{ color: "rgba(255,255,255, 0.6)" }}
+      >
+        * All photos and videos are shared privately. Only parents of the
+        children can access them.
+      </Typography>
+    </CardContent>
+    <CardActions
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "0 0 16px 0",
+      }}
+    >
+      <Button
+        size="large"
+        variant="outlined"
+        href="/"
+        sx={{ color: "white", borderColor: "white" }}
+      >
+        Visit Album
+      </Button>
+    </CardActions>
+  </FixedAspectRatioPaper>
 )
 
 export default OrganizationContactCard
